79bfa98ffb7a24de746c5480735fc62126fad25f
Kyle Constance <kconst@gmail.com>
2016-03-19 10:44:05 -0400

    adding basic column header component

---
+++ js/components/GridHeader.react.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2014-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+var React = require('react');
+var ReactPropTypes = React.PropTypes;
+var GridActions = require('../actions/GridActions');
+
+// var classNames = require('classnames');
+
+var GridHeader = React.createClass({
+
+    propTypes: {
+        column: ReactPropTypes.object.isRequired,
+        index: ReactPropTypes.number.isRequired
+    },
+
+    getInitialState: function () {
+        return {};
+    },
+
+    /**
+     * @return {object}
+     */
+    render: function () {
+        var index = this.props.index,
+            column = this.props.column;
+
+        return (
+            <li index={index} onClick={this._onClick}>
+                {column}
+            </li>
+        );
+    },
+
+    _onClick: function() {
+        GridActions.sortByColumn(this.props.index); 
+    }
+
+});
+
+module.exports = GridHeader;
